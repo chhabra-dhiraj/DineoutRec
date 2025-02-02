48b2a2f62cfb0753e9a3324ef18ffb676632269b
Dhiraj Chhabra <dhiraj.chhabra.g@gmail.com>
2025-02-02 11:38:11 +0530

    chore: add basic project package structure

--- app/src/main/java/io/github/chhabra_dhiraj/dineoutrec/MainActivity.kt
+++ app/src/main/java/io/github/chhabra_dhiraj/dineoutrec/MainActivity.kt
@@ -11,7 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import io.github.chhabra_dhiraj.dineoutrec.ui.theme.DineoutRecTheme
+import io.github.chhabra_dhiraj.dineoutrec.presentation.ui.theme.DineoutRecTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
