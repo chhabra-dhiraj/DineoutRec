f08c7c709f2c2c24e7d898d99cc2b7908a4cfd6a
Dhiraj Chhabra <dhiraj.chhabra.g@gmail.com>
2025-02-07 19:27:28 +0530

    docs: add comment in VenueItemDto

--- app/src/main/java/io/github/chhabra_dhiraj/dineoutrec/data/remote/VenueItemDto.kt
+++ app/src/main/java/io/github/chhabra_dhiraj/dineoutrec/data/remote/VenueItemDto.kt
@@ -3,6 +3,7 @@ package io.github.chhabra_dhiraj.dineoutrec.data.remote
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
+// TODO: Check if it makes sense to have an abstract SectionItemDto for VenueCategorySection items
 @Serializable
 data class VenueItemDto(
     @SerialName("image")
